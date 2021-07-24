2b465e2b567ca1c9970a8d33b601bb9a5e4ec93c
David Rönnlid - Nootralize <dr@nootralize.com>
2021-07-24 20:58:00 +0200

    Installed and setup sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import NumberButton from "./calculatorComponents/numberButtons";
 import ManipulateNumbersButton from "./calculatorComponents/buttonsThatManipulateNumbers";
 
