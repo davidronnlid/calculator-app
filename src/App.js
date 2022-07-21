b3bfbedab7303e6f4a600ed7852b2a42ae49f923
David Rönnlid <david.ronnlid@gmail.com>
2022-07-21 16:26:53 +0200

    Rollback to previous bootstrap version (comp. err)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AppTitle from './aboutProjectComponents/appTitle'
 import Calculator from './calculatorComponents/calculator'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './scss/appStyles.scss'
 
 function App() {
