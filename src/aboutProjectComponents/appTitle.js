864ca61aa3ae20a412bece2571b03ad803882534
David Rönnlid <david.ronnlid@gmail.com>
2022-07-21 15:22:33 +0200

    Added link to portfolio and updated css-var names

--- src/aboutProjectComponents/appTitle.js
+++ src/aboutProjectComponents/appTitle.js
@@ -1,5 +1,15 @@
 const ManipulateNumbersButton = () => {
-  return <h1 className="appTitle">Calculator developed by David Rönnlid</h1>
+  return (
+    <h1 className="appTitle">
+      Calculator developed by{' '}
+      <a
+        href="https://davidronnlidportfolio.netlify.app"
+        className="appTitleLink"
+      >
+        David Rönnlid
+      </a>
+    </h1>
+  )
 }
 
 export default ManipulateNumbersButton
