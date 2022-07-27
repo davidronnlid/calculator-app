c578a25bb7d1ac2265ab0f6be6bc3700be162121
David Rönnlid <david.ronnlid@gmail.com>
2022-07-28 00:52:08 +0200

    Implemented handling of evaluation edge cases

--- src/calculatorComponents/calculator.js
+++ src/calculatorComponents/calculator.js
@@ -1,102 +1,122 @@
-import { useState } from 'react'
-import NumberButton from './numberButtons'
-import ManipulateNumbersButton from './buttonsThatManipulateNumbers'
+import { useState } from "react";
+import NumberButton from "./numberButtons";
+import ManipulateNumbersButton from "./buttonsThatManipulateNumbers";
 
 const Calculator = () => {
-  const [numberList, setNumberList] = useState([])
+  const [numberList, setNumberList] = useState([]);
 
   const addToDisplayArray = (newNumber) => {
-    setNumberList((numberList) => [...numberList, newNumber.toString()])
-  }
+    setNumberList((numberList) => [...numberList, newNumber.toString()]);
+  };
 
   const removeLastNumberFromDisplayArray = () => {
-    numberList.pop()
+    numberList.pop();
 
-    setNumberList((numberList) => [...numberList])
-  }
+    setNumberList((numberList) => [...numberList]);
+  };
   const removeAllNumbersFromDisplayArray = () => {
     setNumberList((numberList) => {
       while (numberList.length > 0) {
-        numberList.pop()
+        numberList.pop();
       }
-      return [...numberList]
-    })
-  }
+      return [...numberList];
+    });
+  };
 
   const onePercentOfCurrentlyDisplayedVal = () => {
     setNumberList((numberList) => {
-      let numberArr = [...numberList]
-      let numberVar = numberArr.join('')
+      let numberArr = [...numberList];
+      let numberVar = numberArr.join("");
 
-      let onePercent = numberVar / 100
+      let onePercent = numberVar / 100;
 
-      onePercent = onePercent.toString().split('')
-      return onePercent
-    })
-  }
+      onePercent = onePercent.toString().split("");
+      return onePercent;
+    });
+  };
 
   const evaluateDisplayVal = () => {
     //Takes numbers before/after a possible user-selected arithmetic operator and carries out the operation in question on these two numbers
 
     setNumberList((numberList) => {
-      let operator, operatorIndex
+      let operator, operatorIndex;
+
+      // const ifMultipleOperators = (numberList, operatorIndex) => {
+      //   if (operatorIndex >= 0) {
+      //     console.log("more than one operator selected");
+      //     alert(
+      //       "Only one operation per evaluation is allowed in this calculator."
+      //     );
+      //     return numberList;
+      //   }
+      // };
 
       for (let i = 0; i < numberList.length; i++) {
+        // ifMultipleOperators(numberList, operatorIndex);
+
         switch (numberList[i]) {
-          case '+':
-            operator = '+'
-            operatorIndex = i
-            break
-          case '/':
-            operator = '/'
-            operatorIndex = i
-            break
-          case '-':
-            operator = '-'
-            operatorIndex = i
-            break
-          case 'X':
-            operator = 'X'
-            operatorIndex = i
-            break
+          case "+":
+            operator = "+";
+            operatorIndex = i;
+            break;
+          case "/":
+            operator = "/";
+            operatorIndex = i;
+            break;
+          case "-":
+            operator = "-";
+            operatorIndex = i;
+            break;
+          case "X":
+            operator = "X";
+            operatorIndex = i;
+            break;
+
           default:
-            break
+            break;
         }
       }
       //Find possible operator and their index in numberList
 
-      if (numberList.length === 0) {
-        return []
-      }
-      // If there aren't any numbers to evaluate, then escape equality evaluation
-
-      const firstVar = parseFloat(numberList.join('').slice(0, operatorIndex))
-      const secondVar = parseFloat(numberList.join('').slice(operatorIndex + 1))
+      const firstVar = parseFloat(numberList.join("").slice(0, operatorIndex));
+      const secondVar = parseFloat(
+        numberList.join("").slice(operatorIndex + 1)
+      );
       //Assign all numbers before/after the operator to separate variables and turn these numbers into type float
 
-      let result
-      if (operator === '+') {
-        result = firstVar + secondVar
-      } else if (operator === '/') {
-        result = firstVar / secondVar
-      } else if (operator === '-') {
-        result = firstVar - secondVar
-      } else if (operator === 'X') {
-        result = firstVar * secondVar
+      let result;
+      if (operator === "+") {
+        result = firstVar + secondVar;
+      } else if (operator === "/") {
+        result = firstVar / secondVar;
+      } else if (operator === "-") {
+        result = firstVar - secondVar;
+      } else if (operator === "X") {
+        result = firstVar * secondVar;
       }
       //Carry out the user-selected operation and assign it to a new variable "result"
 
-      result = [...result.toString()]
-      //Set result to be an array so that the user can continue to add and manipulate numbers in interaction with display (would not be possible if result was still of type number)
-
-      if (result.length > 8) {
-        alert('Operations that return more than 8 digits are not allowed')
-        return ['ERR']
+      if (
+        !numberList.find((elm) => elm === operator) ||
+        !secondVar ||
+        !numberList.length
+      ) {
+        alert("No operations to evaluate");
+        return numberList;
+        // If there are no operators, or if there are no numbers after an operator, or if there are no numbers at all to evaluate, then return unmanipulated numberList
+      } else {
+        result = [...result.toString()];
+        //Set result to be an array so that the user can continue to add and manipulate numbers in interaction with display (would not be possible if result was still of type number)
       }
 
-      return result
-    })
-  }
+      if (result.length > 8) {
+        alert("Operations that return more than 8 digits are not allowed");
+        return ["ERR"];
+      }
+
+      return result;
+    });
+  };
 
   return (
     <div className="calcContainer">
@@ -121,35 +141,35 @@ const Calculator = () => {
         >
           %
         </ManipulateNumbersButton>
-        <ManipulateNumbersButton onClick={() => addToDisplayArray('/')}>
+        <ManipulateNumbersButton onClick={() => addToDisplayArray("/")}>
           /
         </ManipulateNumbersButton>
         <NumberButton onClick={() => addToDisplayArray(7)}>7</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(8)}>8</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(9)}>9</NumberButton>
-        <ManipulateNumbersButton onClick={() => addToDisplayArray('X')}>
+        <ManipulateNumbersButton onClick={() => addToDisplayArray("X")}>
           X
-        </ManipulateNumbersButton>{' '}
+        </ManipulateNumbersButton>{" "}
         <NumberButton onClick={() => addToDisplayArray(4)}>4</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(5)}>5</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(6)}>6</NumberButton>
-        <ManipulateNumbersButton onClick={() => addToDisplayArray('-')}>
+        <ManipulateNumbersButton onClick={() => addToDisplayArray("-")}>
           -
-        </ManipulateNumbersButton>{' '}
+        </ManipulateNumbersButton>{" "}
         <NumberButton onClick={() => addToDisplayArray(1)}>1</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(2)}>2</NumberButton>
         <NumberButton onClick={() => addToDisplayArray(3)}>3</NumberButton>
-        <ManipulateNumbersButton onClick={() => addToDisplayArray('+')}>
+        <ManipulateNumbersButton onClick={() => addToDisplayArray("+")}>
           +
         </ManipulateNumbersButton>
         <NumberButton onClick={() => addToDisplayArray(0)}>0</NumberButton>
-        <NumberButton onClick={() => addToDisplayArray('.')}>.</NumberButton>
+        <NumberButton onClick={() => addToDisplayArray(".")}>.</NumberButton>
         <ManipulateNumbersButton onClick={() => evaluateDisplayVal()}>
           =
         </ManipulateNumbersButton>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
